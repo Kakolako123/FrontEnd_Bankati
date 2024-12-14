2fc4b684834d1df9edeae873da702bd2a61a1fb6
Kakolako123 <kawtar.zouiher.02@gmail.com>
2024-12-14 16:07:34 +0100

    v3

--- src/app/creancier/creancier.component.ts
+++ src/app/creancier/creancier.component.ts
@@ -4,6 +4,13 @@ import { CommonModule, NgClass } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { RouterLink } from "@angular/router";
 
+interface Creditor {
+  name: string;
+  description: string;
+  image: string;
+  category: 'taxes' | 'electricity_water' | 'internet_purchases' | 'all';
+}
+
 @Component({
   selector: 'app-creancier',
   standalone: true,
@@ -16,60 +23,161 @@ import { RouterLink } from "@angular/router";
   templateUrl: './creancier.component.html',
   styleUrl: './creancier.component.css'
 })
-export class CreancierComponent implements AfterViewInit {
+export class CreancierComponent {
+  creditors: Creditor[] = [
+    {
+      name: 'IAM RECHARGES',
+      description: 'Téléphonie et Internet SIM',
+      image: 'assets/facture/maroctelecom.png',
+      category: 'internet_purchases',
+    },
+    {
+      name: 'IAM FACTURES',
+      description: 'Produit Internet SIM, Produit Fixe SIM, Produit Mobile SIM',
+      image: 'assets/facture/maroctelecom.png',
+      category: 'internet_purchases',
+    },
+    {
+      name: 'REDAL',
+      description: 'Factures Redal',
+      image: 'assets/facture/redal.png',
+      category: 'electricity_water',
+    },
+    {
+      name: 'AMENDIS TANGER',
+      description: 'Factures Amendis Tanger',
+      image: 'assets/facture/amendis.png',
+      category: 'electricity_water',
+    },
+    {
+      name: 'LYDEC',
+      description: 'Factures Lydec',
+      image: 'assets/facture/lydec.png',
+      category: 'electricity_water',
+    },
+    {
+      name: 'Marsa Maroc',
+      description: 'Frais portuaires',
+      image: 'assets/facture/Marsa-Maroc.jpg',
+      category: 'taxes',
+    },
+    {
+      name: 'CMA-CGM',
+      description: 'Frais de transport maritime',
+      image: 'assets/facture/cma.png',
+      category: 'taxes',
+    },
+    {
+      name: 'ANCFCC - Droits de conservation',
+      description: 'Paiement des droits de conservation',
+      image: 'assets/facture/ANCFCC - Droits de conservation.jpeg',
+      category: 'taxes',
+    },
+    {
+      name: 'ANCFCC - Consultations et commandes',
+      description: 'Consultation et commandes ANCFCC',
+      image: 'assets/facture/ANCFCC - Droits de conservation.jpeg',
+      category: 'taxes',
+    },
+    {
+      name: 'Ministère de la justice',
+      description: 'Paiement des droits du ministère de la justice',
+      image: 'assets/facture/Ministère de la justice.png',
+      category: 'taxes',
+    },
+    {
+      name: 'CNSS Travailleurs non-salariés',
+      description: 'Paiement des cotisations CNSS',
+      image: 'assets/facture/cnss.png',
+      category: 'taxes'
+    },
+    {
+      name: 'RADEM',
+      description: 'Factures d\'eau et d\'électricité',
+      image: 'assets/facture/radem.jpeg',
+      category: 'electricity_water'
+    },
+    {
+      name: 'RAK',
+      description: 'Factures d\'eau et d\'électricité',
+      image: 'assets/facture/RAK.jpeg',
+      category: 'electricity_water',
+    },
+    {
+      name: 'SRM Souss-Massa (Zone Ex RAMSA)',
+      description: 'Factures d\'eau et d\'électricité',
+      image: 'assets/facture/srm_souss_massa.png',
+      category: 'electricity_water',
+    },
+    {
+      name: 'RADEEF',
+      description: 'Factures d\'eau et d\'électricité',
+      image: 'assets/facture/RADEEF.jpeg',
+      category: 'electricity_water',
+    },
+    {
+      name: 'RADEETA',
+      description: 'Factures d\'eau et d\'électricité',
+      image: 'assets/facture/RADEETA.jpeg',
+      category: 'electricity_water',
+    },
+    {
+      name: 'RADEET',
+      description: 'Factures d\'eau et d\'électricité',
+      image: 'assets/facture/RADEET.jpeg',
+      category: 'electricity_water',
+    },
+    {
+      name: 'Express Relais',
+      description: 'Achat en ligne',
+      image: 'assets/facture/Express Relais.png',
+      category: 'internet_purchases',
+    },
+    {
+      name: 'Winxo',
+      description: 'Achat en ligne',
+      image: 'assets/facture/Winxo.png',
+      category: 'internet_purchases',
+    },
+    {
+      name: 'Royal Air Maroc',
+      description: 'Achat en ligne',
+      image: 'assets/facture/Royal Air Maroc.png',
+      category: 'internet_purchases',
+    },
+    {
+      name: 'AFRIQUIA',
+      description: 'Achat en ligne',
+      image: 'assets/facture/AFRIQUIA.png',
+      category: 'internet_purchases',
+    },
+    {
+      name: 'Atlas Voyages',
+      description: 'Achat en ligne',
+      image: 'assets/facture/atlas voyage.png',
+      category: 'internet_purchases',
+    },
 
-  @ViewChildren('tabButton') tabButtons!: QueryList<ElementRef>;
-  @ViewChildren('billerCard') billerCards!: QueryList<ElementRef>;
-  categorySelect!: ElementRef;
+  ];
 
+  filteredCreditors: Creditor[] = [...this.creditors]; // Initial list is all creditors
+  selectedCategory: 'all' | 'taxes' | 'electricity_water' | 'internet_purchases' = 'all'; // Initial filter category is 'all'
 
-  constructor(private el: ElementRef) {
-  }
-
-  ngAfterViewInit(): void {
-    this.categorySelect = this.el.nativeElement.querySelector('#category-select');
-    this.setupTabs();
-    this.setupCategoryFilter();
-  }
-
-
-  setupTabs(): void {
-    this.tabButtons.forEach((button) => {
-      button.nativeElement.addEventListener('click', () => {
-        const tab = button.nativeElement.getAttribute('data-tab');
-        // Remove active class from all tabs
-        this.tabButtons.forEach((tabButton) =>
-          tabButton.nativeElement.classList.remove('active')
-        );
-        // Add active class to clicked tab
-        button.nativeElement.classList.add('active');
-
-        if (tab === 'list') {
-          //show all billers by default
-          this.billerCards.forEach(card=> card.nativeElement.style.display = '');
-        } else {
-          //TODO: show historical data
-          this.billerCards.forEach(card=> card.nativeElement.style.display = 'none');
-        }
-      });
-    });
-  }
-
-  setupCategoryFilter(): void {
-    if(this.categorySelect && this.categorySelect.nativeElement){
-      this.categorySelect.nativeElement.addEventListener('change', () => {
-        const selectedCategory = (this.categorySelect.nativeElement as HTMLSelectElement).value;
-
-        this.billerCards.forEach(card => {
-          if (selectedCategory === 'all' || card.nativeElement.getAttribute('data-category') === selectedCategory) {
-            card.nativeElement.style.display = '';
-          } else {
-            card.nativeElement.style.display = 'none';
-          }
-        });
-      });
+  // Filter the creditors based on the selected category
+  filterCreditors(category: 'all' | 'taxes' | 'electricity_water' | 'internet_purchases') {
+    this.selectedCategory = category; // Update currently selected category
+    if (category === 'all') {
+      this.filteredCreditors = [...this.creditors]; // Show all creditors
+    } else {
+      this.filteredCreditors = this.creditors.filter(
+        (creditor) => creditor.category === category
+      ); // Filter creditors based on the category
     }
   }
-
+  onFilterChange(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    const selectedValue = target.value as  'all' | 'taxes' | 'electricity_water' | 'internet_purchases';
+    this.filterCreditors(selectedValue);
+  }
 
 }
